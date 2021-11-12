f88d52f4ed6424716ae1bab1e5299de07d9bf5d7
daw2-cano22 <91276972+daw2-cano22@users.noreply.github.com>
2021-11-12 20:56:08 +0100

    Add files via upload

---
+++ commit.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>Commit</title>
+ </head>
+ <body>
+<?php
+$db = mysqli_connect('localhost', 'root', 'Alumno_2021') or die ('Unable to connect. Check your connection parameters.');
+mysqli_select_db($db, 'moviesite') or die(mysqli_error($db));
+switch ($_GET['action']) {case 'add':    switch ($_GET['type']) {    case 'people':        $query = 'INSERT INTO            people                (people_fullname, people_isdirector, people_isactor)            VALUES                ("' . $_POST['people_fullname'] . '",                 ' . $_POST['people_isdirector'] . ',                 ' . $_POST['people_isactor'] . ')';        break;    case 'movie':        $query = 'INSERT INTO movie(movie_name, movie_year, movie_type, movie_leadactor, movie_director)            VALUES                ("' . $_POST['movie_name'] . '",                 ' . $_POST['movie_year'] . ',                 ' . $_POST['movie_type'] . ',                 ' . $_POST['movie_leadactor'] . ',                 ' . $_POST['movie_director'] . ')';        break;    }    break;
+	case 'edit':    switch ($_GET['type']) {    case 'movie':        $query = 'UPDATE movie SET                movie_name = "' . $_POST['movie_name'] . '",                movie_year = ' . $_POST['movie_year'] . ',                movie_type = ' . $_POST['movie_type'] . ',                movie_leadactor = ' . $_POST['movie_leadactor'] . ',                movie_director = ' . $_POST['movie_director'] . '            WHERE movie_id = ' . $_POST['movie_id'];        break;    case 'people':        $query = 'UPDATE people SET                people_fullname = "' . $_POST['people_fullname'] . '",                people_isdirector = ' . $_POST['people_isdirector'] . ',                people_isactor = ' . $_POST['people_isactor'] . '            WHERE people_id = ' . $_POST['people_id'];        break;        }    break;}if (isset($query)) {    $result = mysqli_query($db, $query) or die(mysqli_error($db));}?>  <p>Done!</p> </body></html>
